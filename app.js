d9a374783064e7a6a33c959c824f905eb0a6892f
santiyamani123 <santhiya.softwiz@gmail.com>
2022-09-05 12:58:04 +0530

    variable declartion in typescript

---
+++ app.js
@@ -0,0 +1,69 @@
+// function add(a:number,b:number):number
+// {
+//     return a+b;
+// }
+// console.log(add(1,2));
+// var concept
+// var a = 10;
+// console.log(window.a);
+// let a = 10;
+// console.log(window.a);
+// var a = 10;
+// var a = 20;
+// console.log(a);
+// let a = 10;
+// let a = 20;
+// console.log(a);
+// var a = 10;
+// function f()
+// {
+//     console.log(a);
+// }
+// f();
+// within function 
+// var a = 10;
+// function f()
+// {
+//     var a = 20;
+//     return a;
+// }
+// console.log(f());
+// within anther  function 
+// function f()
+// {
+//     var a = 10;
+//     return function g(){
+//         var b =a+1;
+//         return b;
+//     };
+//     }
+//     var g = f();
+//     console.log(g());
+// function f(shouldInitialized:boolean):number | undefined
+// {
+//     if(shouldInitialized)
+//     {
+//         var x = 10;
+//     }
+//     return x;
+// }
+// console.log(f(true));
+// console.log(f(false));
+// Duplicate identifier  for let
+// function f(d:number)
+// {
+//     let d = 10;
+//     return d;
+// }
+// f(1);
+function sumMatrix(matrix) {
+    var sum = 0;
+    for (var i = 0; i < matrix.length; i++) {
+        var currentRow = matrix[i];
+        for (var i_1 = 0; i_1 < matrix.length; i_1++) {
+            sum + currentRow[i_1];
+        }
+        return sum;
+    }
+}
+console.log(sumMatrix([[1, 2, 3], [4, 5, 6]]));
